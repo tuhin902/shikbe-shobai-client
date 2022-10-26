07b59edafa270a28f6be82754c96e257ca0d0002
tuhin902 <tuhin902md@gmail.com>
2022-10-27 00:18:56 +0600

    right side nav added

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FaGoogle, FaGithub } from "react-icons/fa";
 
+
 const RightSideNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/course-categories')
+            .then(res => res.json())
+            .then(data => setCategories(data));
+    }, [])
+
+
     return (
         <div>
-            <button className="btn btn-outline btn-info mb-4"><span className='mr-2'><FaGoogle></FaGoogle></span> Login with Google</button>
-            <br />
-            <button className="btn btn-outline btn-success"><span className='mr-2'><FaGithub></FaGithub></span>Login with Github</button>
+            <div className='pr-20'>
+                <h3 className='text-3xl mb-2'>All category</h3>
+                <div>
+                    {
+                        categories.map(category => <p key={category.id} className='mb-2' >
+                            <Link to={`/category/${category.id}`}>{category.name}</Link>
+                        </p>)
+                    }
+                </div>
+            </div>
+            <div className='my-10'>
+                <button className="btn btn-outline btn-info mb-4"><span className='mr-2'><FaGoogle></FaGoogle></span> Login with Google</button>
+                <br />
+                <button className="btn btn-outline btn-success"><span className='mr-2'><FaGithub></FaGithub></span>Login with Github</button>
+            </div>
 
         </div>
     );
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Header from '../Pages/Shared/Header/Header';
-import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
 import RightSideNav from '../Pages/Shared/RightSideNav/RightSideNav';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <div className='grid grid-cols-5 gap-6'>
-                <LeftSideNav></LeftSideNav>
-                <div className='col-span-3'>
+            <div className='grid grid-cols-4 gap-6 mt-5'>
+                <div className='col-span-3 ml-10'>
                     <Outlet></Outlet>
                 </div>
                 <RightSideNav></RightSideNav>
