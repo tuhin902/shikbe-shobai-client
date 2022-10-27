b704554412f4b7d087ce77363831db463620f428
tuhin902 <tuhin902md@gmail.com>
2022-10-27 19:23:40 +0600

    blogs first ans added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h3>blogs</h3>
+            <div className='border border-indigo-500 w-1/2 mx-10 my-20 p-5 rounded-xl'>
+                <h3 className='text-2xl bold'>what is cors?</h3>
+                <p>
+                    Node.js
+                    API
+                    Node.js is an open-source and cross-platform runtime used when executing JavaScript code on the server-side. One of the popular Node.js server frameworks is Express. Implementing CORS in Node.js helps you access numerous functionalities on the browser.
+
+                    Express allows you to configure and manage an HTTP server to access resources from the same domain.</p>
+            </div>
+            <div></div>
+            <div></div>
         </div>
     );
 };
